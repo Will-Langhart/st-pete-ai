9d38ec695b38f0d58ee914f95fbcba7af803cacf
Will Langhart <theholotrout@gmail.com>
2024-05-03 11:53:28 -0400

    Update app.py
    Signed-off-by: Will Langhart <theholotrout@gmail.com>

--- app.py
+++ app.py
@@ -115,8 +115,7 @@ if __name__ == '__main__':
      app.run(
         host='0.0.0.0',
         port=5000,
-        debug=True,
-    }
+        debug=True
 
 llm = ChatOpenAI()
 llm.invoke("Hello, world!")
