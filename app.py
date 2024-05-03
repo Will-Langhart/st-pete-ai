6cb01ef25e2fe7d57e02250b9acb974908cd0fbb
Will Langhart <theholotrout@gmail.com>
2024-05-03 11:52:59 -0400

    Update app.py
    Signed-off-by: Will Langhart <theholotrout@gmail.com>

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(
+     app.run(
         host='0.0.0.0',
         port=5000,
         debug=True,
